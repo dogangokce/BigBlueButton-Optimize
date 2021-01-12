c254f5812556c35379807c9fd0e989f1816742b9
Dogan GOGCE <dogangokce76@gmail.com>
2021-01-12 18:07:10 +0300

    Update replace-config.sh

--- replace-config.sh
+++ replace-config.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo "Replacing default apply-conf.sh in BBB with your customized version"
+echo "BBB'deki varsayılan apply-conf.sh'yi özelleştirilmiş sürümünüzle değiştirme"
 cp apply-config.sh /etc/bigbluebutton/bbb-conf/apply-config.sh
 
-echo "Restarting bbb"
+echo "bbb yeniden başlatılıyor"
 bbb-conf --restart
