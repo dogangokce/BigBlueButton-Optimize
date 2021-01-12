ed7b6e5352b0d53811474a23b758cf6f7fbd575a
Dogan GOGCE <dogangokce76@gmail.com>
2021-01-12 18:33:47 +0300

    Add files via upload

---
+++ firma2/kur.sh
@@ -0,0 +1,12 @@
+git clone https://github.com/dogangokce/BigBlueButton-Optimize.git
+cd BigBlueButton-Optimize/firma2
+cp apply-config-sample.sh apply-config.sh
+chmod 755 replace-config.sh
+./replace-config.sh
+cp default.pdf /var/www/bigbluebutton-default/
+cp favicon.ico /var/www/bigbluebutton-default/
+cp default.pptx /var/www/bigbluebutton-default/
+cp index.html /var/www/bigbluebutton-default/
+cp tr_TR.json /usr/share/meteor/bundle/programs/server/assets/app/locales/
+cp logo.png /var/bigbluebutton/playback/presentation/2.0/
+bbb-conf --restart
