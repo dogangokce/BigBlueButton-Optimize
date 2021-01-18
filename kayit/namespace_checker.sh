37e99954f782d4e5685cce57105fe4d5ffa679e5
Dogan GOGCE <dogangokce76@gmail.com>
2021-01-18 18:29:47 +0300

    Update namespace_checker.sh

--- kayit/namespace_checker.sh
+++ kayit/namespace_checker.sh
@@ -4,8 +4,8 @@ VALUE=$(cat /boot/config-$(uname -r) | grep CONFIG_USER_NS)
 
 if [[ -z "$VALUE" ]]
 then
-  echo 'You do not have namespacing in the kernel. You will need to enable the SUID sandbox or upgrade your kernel.'
-  echo 'See https://chromium.googlesource.com/chromium/src/+/master/docs/linux_suid_sandbox_development.md'
+  echo 'Çekirdekte ad alanınız yok. SUID korumalı alanını etkinleştirmeniz veya çekirdeğinizi yükseltmeniz gerekecektir.'
+  echo 'Bakın https://chromium.googlesource.com/chromium/src/+/master/docs/linux_suid_sandbox_development.md'
   exit 1
 fi
 
@@ -15,7 +15,7 @@ if [[ "$USER_NS_AVAILABLE" == "y" ]]
 then
   exit 0
 else
-  echo 'You do not have namespacing in the kernel. You will need to enable the SUID sandbox or upgrade your kernel.'
-  echo 'See https://chromium.googlesource.com/chromium/src/+/master/docs/linux_suid_sandbox_development.md'
+  echo 'Çekirdekte ad alanınız yok. SUID korumalı alanını etkinleştirmeniz veya çekirdeğinizi yükseltmeniz gerekecek.'
+  echo 'Bakın https://chromium.googlesource.com/chromium/src/+/master/docs/linux_suid_sandbox_development.md'
   exit 1
 fi
