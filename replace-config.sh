304c026cd9258c45dcdfb152e0b01ad782d83ff7
Dogan GOGCE <dogangokce76@gmail.com>
2021-01-18 18:10:17 +0300

    Delete replace-config.sh

--- replace-config.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "BBB'deki varsayılan apply-conf.sh'yi özelleştirilmiş sürümünüzle değiştirme"
-cp apply-config.sh /etc/bigbluebutton/bbb-conf/apply-config.sh
-
-echo "bbb yeniden başlatılıyor"
-bbb-conf --restart
