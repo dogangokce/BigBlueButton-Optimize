a37d97d123e1b1e36412db93ec75e83e892f0ea8
Dogan GOGCE <dogangokce76@gmail.com>
2021-01-19 01:01:06 +0300

    Update kur.sh

--- firma1/kur.sh
+++ firma1/kur.sh
@@ -1,12 +1,12 @@
-git clone https://github.com/dogangokce/BigBlueButton-Optimize.git
-cd BigBlueButton-Optimize/firma1
-cp apply-config-sample.sh apply-config.sh
-chmod 755 replace-config.sh
+RUN git clone https://github.com/dogangokce/BigBlueButton-Optimize.git
+RUN cd BigBlueButton-Optimize/firma1
+RUN cp apply-config-sample.sh apply-config.sh
+RUN chmod 755 replace-config.sh
 ./replace-config.sh
-cp default.pdf /var/www/bigbluebutton-default/
-cp favicon.ico /var/www/bigbluebutton-default/
-cp default.pptx /var/www/bigbluebutton-default/
-cp index.html /var/www/bigbluebutton-default/
-cp tr_TR.json /usr/share/meteor/bundle/programs/server/assets/app/locales/
-cp logo.png /var/bigbluebutton/playback/presentation/2.0/
-bbb-conf --restart
+RUN cp default.pdf /var/www/bigbluebutton-default/
+RUN cp favicon.ico /var/www/bigbluebutton-default/
+RUN cp default.pptx /var/www/bigbluebutton-default/
+RUN cp index.html /var/www/bigbluebutton-default/
+RUN cp tr_TR.json /usr/share/meteor/bundle/programs/server/assets/app/locales/
+RUN cp logo.png /var/bigbluebutton/playback/presentation/2.0/
+RUN sudo bbb-conf --restart
