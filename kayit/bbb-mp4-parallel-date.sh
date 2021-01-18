b815717f0fce2dc79671cbf1cc9ec11f15744c79
Dogan GOGCE <dogangokce76@gmail.com>
2021-01-18 18:35:20 +0300

    Update bbb-mp4-parallel-date.sh

--- kayit/bbb-mp4-parallel-date.sh
+++ kayit/bbb-mp4-parallel-date.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 n=0
 
-#Give date range of recordings for which you want to create MP4 files. 
-#To create MP4 files of 29 Sep, from_date should be 29 Sep and to_date should be 30 Sep i.e. 1 day additional
-echo "Converting recordings from $from_date to $to_date"
+#MP4 dosyaları oluşturmak istediğiniz kayıtların tarih aralığını belirtin.
+#29 Eylül MP4 dosyaları oluşturmak için, başlangıç_tarihi 29 Eylül ve to_date 30 Eylül, yani 1 gün ek olmalıdır
+echo "Kayıtları $from_date den $to_date e dönüştürme"
 from_date='29 Sep 2020'
 to_date='30 Sep 2020'
 
-#Path of recordings is /mnt/scalelite-recordings/var/bigbluebutton/published/presentation/ if you execute this script on  Scalelite
-#Path of recordings is /var/bigbluebutton/published/presentation if you execute this script on BBB
+#Kayıtların yolu /mnt/scalelite-recordings/var/bigbluebutton/published/presentation/ bu komut dosyasını Scalelite üzerinde çalıştırırsanız
+#Kayıtların yolu  /var/bigbluebutton/published/presentation bu komut dosyasını BBB üzerinde çalıştırırsanız
 recordings_directory='/mnt/scalelite-recordings/var/bigbluebutton/published/presentation/'
-echo "Converting recordings from $recordings_directory"
+echo "$recordings_directory den kayıtları dönüştürme"
 
-#File where unprocessed recordings will be added. We will convert these recordings into MP4.
+#İşlenmemiş kayıtların ekleneceği dosya. Bu kayıtları MP4'e dönüştüreceğiz.
 filename='bbb-unprocessed-recordings.txt'
-#Ensure that the file exists and is empty
+#Dosyanın var olduğundan ve boş olduğundan emin olun
 :> "$filename"
 
 for i in $(find "$recordings_directory" -maxdepth 1 -newerct "$from_date" ! -newerct "$to_date" -printf "%f\n"); do
@@ -22,5 +22,5 @@ for i in $(find "$recordings_directory" -maxdepth 1 -newerct "$from_date" ! -new
     [ ! -f "$recording" ] && n=$((n+1)) && echo "#$n $recording not found" && echo "$i" >> "$filename"
 done
 
-echo "Executing 2 MP4 conversion jobs in parallel";
+echo "Paralel olarak 2 MP4 dönüştürme işinin yürütülmesi";
 parallel -j 2 --joblog log/parallel_mp4.log --resume-failed -a "$filename" node bbb-mp4 &
